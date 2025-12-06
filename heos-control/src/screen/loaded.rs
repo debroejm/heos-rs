3a050c209d12b3e75cfb1d8b1efeeffbd73c242b
James M De Broeck <debroejm@gmail.com>
2025-12-05 16:56:05 -0800

    feat: tidy up and add selection visuals for sidebar

--- heos-control/src/screen/loaded.rs
+++ heos-control/src/screen/loaded.rs
@@ -1,6 +1,8 @@
-use egui::{Button, Context, Margin, Rgba, Stroke};
+use egui::{Button, Color32, Context, Image, Margin, Response, Rgba, Stroke, Ui, Widget};
 use heos::{HeosConnection, Stateful};
 use std::sync::Arc;
+use emath::Vec2;
+use strum::{EnumDiscriminants, IntoDiscriminant};
 
 use crate::actions::Actions;
 use crate::assets;
@@ -9,9 +11,66 @@ use crate::screen::media_bar::MediaBar;
 use crate::screen::queue::QueueScreen;
 use crate::updater::Updater;
 
+#[derive(EnumDiscriminants)]
+#[strum_discriminants(name(ScreenType))]
 enum Screen {
     Devices(Devices),
     Queue(QueueScreen),
+}
+
+impl ScreenType {
+    fn image(&self) -> Image<'static> {
+        match self {
+            Self::Devices => assets::icons::devices::image(),
+            Self::Queue => assets::icons::queue::image(),
+        }
+    }
+}
+
+struct SidePanelButton {
+    screen_type: ScreenType,
+    enabled: bool,
+    selected: bool,
+}
+
+impl SidePanelButton {
+    fn new(screen_type: ScreenType) -> Self {
+        Self {
+            screen_type,
+            enabled: true,
+            selected: false,
+        }
+    }
+
+    fn enabled(mut self, enabled: bool) -> Self {
+        self.enabled = enabled;
+        self
+    }
+
+    fn selected(mut self, screen: &Screen) -> Self {
+        self.selected = screen.discriminant() == self.screen_type;
+        self
+    }
+}
+
+impl Widget for SidePanelButton {
+    fn ui(self, ui: &mut Ui) -> Response {
+        ui.scope(|ui| {
+            ui.spacing_mut().button_padding = Vec2::new(4.0, 4.0);
+            let visuals = ui.visuals_mut();
+            visuals.button_frame = true;
+            visuals.widgets.inactive.weak_bg_fill = Color32::TRANSPARENT;
+            visuals.widgets.hovered.bg_stroke = Stroke::NONE;
+            visuals.widgets.active.bg_stroke = Stroke::NONE;
+            visuals.selection.stroke = Stroke::new(1.0, Color32::WHITE);
+
+            let button = Button::new(self.screen_type.image())
+                .image_tint_follows_text_color(true)
+                .corner_radius(0.0)
+                .selected(self.selected);
+            ui.add_enabled(self.enabled, button)
+        }).inner
+    }
 }
 
 pub struct Loaded {
@@ -52,21 +111,21 @@ impl Loaded {
             .show_separator_line(false)
             .show(ctx, |ui| {
                 ui.vertical(|ui| {
-                    let devices_button = Button::new(assets::icons::devices::image())
-                        .image_tint_follows_text_color(true)
-                        .fill(Rgba::from_luminance_alpha(0.0, 0.0))
-                        .stroke(Stroke::NONE);
+                    ui.spacing_mut().item_spacing.y = 4.0;
+                    ui.add_space(4.0);
+
+                    let devices_button = SidePanelButton::new(ScreenType::Devices)
+                        .selected(&self.screen);
                     if ui.add(devices_button).clicked() {
                         let devices = Devices::new(self.heos.clone(), updater);
                         self.screen = Screen::Devices(devices);
                     }
 
                     let playable_id = self.media_bar.playable_id();
-                    let queue_button = Button::new(assets::icons::queue::image())
-                        .image_tint_follows_text_color(true)
-                        .fill(Rgba::from_luminance_alpha(0.0, 0.0))
-                        .stroke(Stroke::NONE);
-                    if ui.add_enabled(playable_id.is_some(), queue_button).clicked() {
+                    let queue_button = SidePanelButton::new(ScreenType::Queue)
+                        .selected(&self.screen)
+                        .enabled(playable_id.is_some());
+                    if ui.add(queue_button).clicked() {
                         if let Some(playable_id) = playable_id {
                             let queue = QueueScreen::new(self.heos.clone(), updater, playable_id);
                             self.screen = Screen::Queue(queue);
