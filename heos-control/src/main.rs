ac194e786163873f0f41b6b20e3ee6dc71d1040a
James M De Broeck <debroejm@gmail.com>
2025-12-19 20:38:47 -0800

    fix: remove force-enabled console for Windows builds

--- heos-control/src/main.rs
+++ heos-control/src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
+
 use eframe::epaint::FontFamily;
 use eframe::{CreationContext, Frame};
 use egui::{Context, FontId, RichText, TextStyle, ViewportCommand};
